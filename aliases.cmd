cf667ced41c5b68a1a5afedd7bca9d3c40700f06
skwzrd <39067332+skwzrd@users.noreply.github.com>
2020-12-15 14:25:33 -0500

    feat: windows aliases

---
+++ aliases.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+DOSKEY gpm=git push --set-upstream origin master
+DOSKEY gpd=git push --set-upstream origin dev
+
+DOSKEY gcd=git checkout dev
+DOSKEY gcm=git checkout master
+
+DOSKEY gs=git status
+DOSKEY ga=git add .
+DOSKEY gr=git remote add origin
+
+DOSKEY ls=dir
